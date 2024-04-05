fe4a9ab3709da1002f31b4d65c83f08392cad296
danielagmz <161260538+danielagmz@users.noreply.github.com>
2024-04-05 12:41:33 +0200

    codigo documentado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 import java.awt.*;
 
+/**
+ * clase principal del programa
+ * @author daniela
+ * @data 05/04/2024
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * clase main del programa
+     * @author daniela
+     * @data 05/04/2024
+     * @version 1.0
+     */
     public static void main(String[] args) {
         new Menu().menuPrincipal();
     }
--- src/Menu.java
+++ src/Menu.java
@@ -8,12 +8,33 @@
 import java.util.Scanner;
 import java.lang.Math;
 
+/**
+ * clase principal del proyecto
+ * @author daniela
+ * @data 05/04/2024
+ * @version 1.0
+ */
 public class Menu {
+    /**
+     * variable de lectura por teclado
+     */
     private Scanner in = new Scanner(System.in);
+    /**
+     * variable de seleccion del menu
+     */
     private int opcio;
+    /**
+     * vector que contiene 10 numeros
+     */
     private int [] vector = new int[10];
+    /**
+     * matriz de 10x10
+     */
     private int [][] matriu = new int[10][10];
 
+    /**
+     * funcion que gestiona el menu de opciones del programa
+     */
     public void menuPrincipal() {
         do {
             System.out.println("1. Crear un vector");
@@ -59,6 +80,11 @@ public class Menu {
         }while(opcio != 0);
     }
 
+    /**
+     * metodo que crea un vector de 10 numeros aleatorios en un rango de 0 a un numero entrado por taclado
+     * @param vector el vector a rellenar
+     * @param n indica el largo del vector
+     */
 
     public static void crearVector(int []vector,int n){
         for (int i = 0; i < n; i++) { //omplim vector
@@ -66,6 +92,11 @@ public class Menu {
             System.out.print(vector[i]);
         }
     }
+
+    /**
+     * busca numeros pares o impares en la matriz que se le pase, luego imprime de cual hay mas o "empate" si hay igual cantidad
+     * @param matriu matriz en la que buscar
+     */
     public static void parellsoSenars(int [][]matriu){
         //ToDo: aquesta funció ha de retornar el total de números parells o senars segons qui guanyi
         int parell=0,senar=0;
@@ -88,17 +119,30 @@ public class Menu {
         }
         else System.out.println("empat!");
     }
+
+    /**
+     * busca numeros pares en el vector proporcionado
+     * @param vector vector en el que buscar
+     * @return retorna true luego de imprimir si hay o no numeros pares
+     */
     public static boolean hihaParells(int [] vector){
         int i=0;
         while ((vector[i]%2 != 0) && (i <= vector.length)){  //v.length ens diu la longitud del vector
             i=i+1;
         }
+
         if (i != vector.length){
             System.out.println("hi ha almenys un número parell");
         }
         else System.out.println("no s'ha trobat cap número parell");
+
         return true;
     }
+
+    /**
+     * muestra el contenido del vector que se le pase
+     * @param vector indica el vector a tratar
+     */
     public static void mostrarContingut(int [] vector){
         //PENDENT: aquesta funció està condicionada a l'execució d'alguna  de les funcions anteriors
         int i=0;
